d52f526c9d0124849e9d8708ea2f2f6ecd9bc589
Vikas Rai <vikas.rai@mhealth.tech>
2025-04-09 01:57:03 +0530

    checkpoint-05

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -178,7 +178,7 @@ function Form({
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
             >
-                Connect
+                Report
             </motion.button>
         </motion.form>
     )
